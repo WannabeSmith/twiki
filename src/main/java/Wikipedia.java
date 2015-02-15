f6e8d327366fcd0e0409c68332e2f09449e2ca88
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-15 02:27:39 -0500

    Natural language began.

--- src/main/java/Wikipedia.java
+++ src/main/java/Wikipedia.java
@@ -1,78 +1,101 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Scanner;
 
 public class Wikipedia {
-	
+
 	private String searchTerm;
-	
+
 	public Wikipedia(String msg) {
 		searchTerm = msg;
 	}
-	public String getSummary() throws IOException{
+
+	public String search(String query) throws IOException {
+		String encodedUrl  = "";
 		
-		URL url = new URL(makeUrl(searchTerm));
-		
-		try 
-		{
-			URLConnection conn = url.openConnection();
-	        conn.connect();
-	        InputStream is = conn.getInputStream();	
-	        
-	        String data = convertStreamToString(is);
-	        data = parseData(data);
-	        is.close();
-	        return data;
+		try {
+		    encodedUrl = URLEncoder.encode(query, "UTF-8");
+		} catch (UnsupportedEncodingException ignored) {
+		    // Can be safely ignored because UTF-8 is always supported
 		}
-		catch (Exception e)
-		{
-			return "Error calling Wikipedia API:\n\n" + e;
-		}
+		URL url = new URL(makeUrl(encodedUrl));
+		searchTerm = encodedUrl;
+		URLConnection conn = url.openConnection();
+		conn.connect();
+		InputStream is = conn.getInputStream();
+		String data = convertStreamToString(is);
+		is.close();
+		return data;
 	}
-	
+
+	public String getSummary() throws IOException {
+		return getSummary(0);
+	}
+
+	private boolean checkMultiples(String data) throws IOException {
+
+		if (data.contains(searchTerm+ " may refer to:")) {
+			return true;
+		}
+		return false;
+	}
+
+	public String getSummary(int sentences) throws IOException {
+
+		String data = search(searchTerm);
+		data = parseData(data);
+		if (!checkMultiples(data)) {
+
+			if (sentences > 0) {
+				data = getString(sentences, data);
+			}
+
+			return data;
+		}
+		return "More than one option";
+	}
+
 	private String convertStreamToString(java.io.InputStream inputstream) {
-	   Scanner s = new java.util.Scanner(inputstream).useDelimiter("\\A");
-	    return s.hasNext() ? s.next() : "";
+		Scanner s = new java.util.Scanner(inputstream).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
 	}
-    
-    private String parseData(String file) throws IOException {
-    	   int indexnum = file.indexOf("extract");
-    	   String extract = file.substring(indexnum+10, file.length()-5);
-    	   String extractNoMarkUp = removeMarkUp(extract);
-    	   return extractNoMarkUp;
-    }
-    
-    private String removeMarkUp(String file)  {
-    	file = file.replace("\\u", "");
-    	file = file.replace("\\b", "");
-    	file = file.replace("\\n", "");
-    	file = file.replace("\\", "");
-		return file;	
-    }
-    
-    private String getString(int numString, String name)
-    {
-    	int k =0;
-    	int index = 0;
-    	for (int i = 0; k < numString; i++)
-    	{
-    		if (name.charAt(i) == '.' )
-    		{
-    			k++;
-    		}
-    		index++;
-    	}
-    	
-    	name = name.substring(0,index);
+
+	private String parseData(String file) throws IOException {
+		int indexnum = file.indexOf("extract");
+		String extract = file.substring(indexnum + 10, file.length() - 5);
+		String extractNoMarkUp = removeMarkUp(extract);
+		return extractNoMarkUp;
+	}
+
+	private String removeMarkUp(String file) {
+		file = file.replace("\\u", "");
+		file = file.replace("\\b", "");
+		file = file.replace("\\n", "");
+		file = file.replace("\\", "");
+		return file;
+	}
+
+	private String getString(int numString, String name) {
+		int k = 0;
+		int index = 0;
+		for (int i = 0; k < numString; i++) {
+			if (name.charAt(i) == '.') {
+				k++;
+			}
+			index++;
+		}
+
+		name = name.substring(0, index);
 		return name;
-    	
-    }
-    
-    
-    private String makeUrl(String name) {
-    	return "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+name;
-    }
+
+	}
+
+	private String makeUrl(String name) {
+		return "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&continue=&titles="
+				+ name;
+	}
 }
